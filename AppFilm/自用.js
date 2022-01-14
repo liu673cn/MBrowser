3a976de1dff56dc152e1a55caa20cc443d72f895
272775028@qq.com <272775028@qq.com>
2022-01-14 20:17:06 +0800

    update

--- AppFilm/自用.js
+++ AppFilm/自用.js
@@ -502,12 +502,10 @@ var uu=getVar("url");
 var resp=JZ(JSON.stringify({url:uu,redirect:false,head:{"User-Agent":"Mozilla/5.0 Android"}}));
 if(uu.indexOf("baidu.com")!=-1){
     var playurl=uu.split("wd=")[1];
-    if(playurl.indexOf("duoduozy.com")!=-1){
-        var uuu="http://json.yyyy.fun/xd.php?url="+playurl;
+    if(playurl.indexOf("duoduozy.com")!=-1||playurl.indexOf("suoyo.cc")!=-1){
+        var uuu="https://www.6080kan.cc/app.php?url="+playurl;
         var resp=JZ(JSON.stringify({url:uuu}));
         JSON.stringify({url:JSON.parse(resp.code).url,head:{"referer":"https://player.duoduozy.com"}});
-    }else if(playurl.indexOf("suoyo.cc")!=-1){
-        JSON.stringify({url:playurl,head:{"referer":"origin: https://player.duoduozy.com"}});
     }else if(playurl.indexOf("api.iopenyun.com:88")!=-1){
         if(playurl.indexOf("html")!=-1){
             var uu="https://api.m3u8.tv:5678/home/api?type=ys&uid=233711&key=dgilouvFKNRSWX2467&url="+playurl.split("=")[1];
