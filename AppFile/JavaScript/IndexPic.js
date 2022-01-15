ae425996e0e7106cedbc103b395fd8694403bd53
InMemory <272775028@qq.com>
2022-01-15 10:52:55 +0800

    update

--- AppFile/JavaScript/IndexPic.js
+++ AppFile/JavaScript/IndexPic.js
@@ -5,7 +5,6 @@ var NewVersion="20211120";
 var version=e2Rex(getVar("QMINFO"),".xml(version).z(\\d+)");
 var appSign="8c1a742e9f9c4ee5b9ec18fa642b32c5";
 var sign=e2Rex(getVar("QMINFO"),".xml(sign).t()").replace(/\s/g,"");
-var filename=e2Rex("aHR0cHM6Ly9pbm1lbW9yeS5jb2RpbmcubmV0L3AvSW5NZW1vcnkvZC9BcHBGaWxtL2dpdC9yYXcvbWFzdGVyL+WIl+ihqC5qc29u",".t().dn64()");
 var appName="APP影视.js";
 var name=e2Rex(getVar("QMINFO"),".xml(name).t()").replace(/\s/g,"");
 if(version==NewVersion&&sign==appSign&&name==appName){
