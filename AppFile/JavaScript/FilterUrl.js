cec6e4584417b2470f3181095495d70d8f829634
InMemory <272775028@qq.com>
2022-01-15 22:06:14 +0800

    删除 JavaScript

--- AppFile/JavaScript/FilterUrl.js
+++
@@ -1,9 +0,0 @@
-var code=getVar("CODE");
-var URL=e2Rex(code,".json(url)");
-if(URL.indexOf("api.php/app")!=-1||URL.indexOf("xgapp.php/v1")!=-1){
-    URL+"nav?token=";
-}else if(URL.indexOf(".vod")!=-1){
-    URL+"/types";
-}else{
-    "";
-}
