3b100fffdf87f078d4292ee07f2c5002c7a90922
272775028@qq.com <272775028@qq.com>
2022-03-30 02:18:49 +0800

    update

--- AppFile/APP影视.js
+++ AppFile/APP影视.js
@@ -132,7 +132,11 @@ var URL=e2Rex(code,".json(url)");
 if(URL.indexOf("api.php/app")!=-1||URL.indexOf("xgapp")!=-1){
     "";
 }else if(URL.indexOf(".vod")!=-1){
+  if(URL.indexOf("youguo520")!=-1){
+    "apikey=7669d8a4f1776e046e9a44f40b522137";
+  }else{
     "";
+  }
 }else if(URL.indexOf("豆瓣")!=-1){
     "host=frodo.douban.com&accept=*/*";
 }else{
@@ -144,11 +148,7 @@ var URL=e2Rex(code,".json(url)");
 if(URL.indexOf("api.php/app")!=-1||URL.indexOf("xgapp")!=-1){
     "";
 }else if(URL.indexOf(".vod")!=-1){
-  if(URL.indexOf(youguo520)!=-1){
-    "apikey=7669d8a4f1776e046e9a44f40b522137";
-  }else{
     "";
-  }
 }else if(URL.indexOf("搜狗")!=-1){
     "ABTEST=3|1615358299|v1; IPLOC=CN4403; SUV=00F0551665E97E5C6048695B5B6E5290; VIDEO_DEBUG=off";
 }else{
