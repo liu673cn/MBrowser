fd018c0bb7083edd4f6f1859354facc550971ef7
InMemory <272775028@qq.com>
2022-04-05 21:22:45 +0800

    update

--- AppFile/test.js
+++ AppFile/test.js
@@ -807,19 +807,26 @@ JSON.stringify(items);
 ######删除规则25
 eval(e2Rex(getHttp('https://inmemory.coding.net/p/InMemory/d/MBrowser/git/raw/master/AppFile/js/q.js'),'.dn64()'));
 var SubName=getVar("订阅名");
-var filename=SubName+'.json';
-var 记录=getVar("CODE");
-var 新记录=JSON.parse(_.read(filename));
-let res=新记录.some(item=>{
-    if(item.title == JSON.parse(记录).type){
-        item.data=item.data.filter(a=>a.url!=JSON.parse(记录).url);
-        return true
-    }
-});
-var AppName=e2Rex(记录,".json(title)");
-_.write(JSON.stringify(新记录),filename);
-alert(AppName+"\n删除成功");
-_.read(filename);
+if(SubName>1){
+    var filename=SubName+'.json';
+    var 记录=getVar("CODE");
+    var 新记录=JSON.parse(_.read(filename));
+    let res=新记录.some(item=>{
+        if(item.title == JSON.parse(记录).type){
+            item.data=item.data.filter(a=>a.url!=JSON.parse(记录).url);
+            return true
+        }
+    });
+    var AppName=e2Rex(记录,".json(title)");
+    _.write(JSON.stringify(新记录),filename);
+    alert(AppName+"\n删除成功");
+    _.read(filename);
+}else{
+    var filename='远程订阅索引.txt';
+    var 新记录=_.read(filename).match(/.+?,.+/g).filter(item=>item!=getVar("url"));
+    _.write(新记录.join("\n"),filename);
+    _.read(filename);
+}
 ######本地规则批量搜索25
 eval(e2Rex(getVar("qjs"),'.dn64()'));
 var filename='APP影视.json';
