21bee5b2b013f29aa93aec75929a372d2fae2dd9
272775028@qq.com <272775028@qq.com>
2022-04-05 16:35:54 +0800

    update

--- AppFile/test.js
+++ AppFile/test.js
@@ -773,6 +773,7 @@ JSON.stringify(items);
 eval(e2Rex(getHttp('https://inmemory.coding.net/p/InMemory/d/MBrowser/git/raw/master/AppFile/js/q.js'),'.dn64()'));
 var filename='远程订阅索引.txt';
 var code=_.read(filename).match(/.+?,.+/g)+',本地,http://www.cx99999.cn/lvdou_api.php/v1.vod#';
+alert(code);
 var items=[];
 for (var i in code){
     var title=e2Rex(code[i],".tz(,)");
