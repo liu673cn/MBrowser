cf818eb9c83bfaf5862b79f011665e0cdbed12f6
272775028@qq.com <272775028@qq.com>
2022-04-05 16:04:46 +0800

    update

--- AppFile/test.js
+++ AppFile/test.js
@@ -773,7 +773,7 @@ JSON.stringify(items);
 eval(e2Rex(getHttp('https://inmemory.coding.net/p/InMemory/d/MBrowser/git/raw/master/AppFile/js/q.js'),'.dn64()'));
 var filename='远程订阅索引.txt';
 var filecode=_.read(filename).match(/.+?,.+/g);
-var code=fcode+"本地,本地规则#";
+var code=filecode+"本地,本地规则#";
 var items=[];
 for (var i in code){
     var title=e2Rex(code[i],".tz(,)");
