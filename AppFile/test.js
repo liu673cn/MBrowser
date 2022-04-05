26285ef94db3f70f760875bef0994ecf7fa61a4b
InMemory <272775028@qq.com>
2022-04-05 21:00:30 +0800

    update

--- AppFile/test.js
+++ AppFile/test.js
@@ -806,7 +806,7 @@ for (var i in code){
 JSON.stringify(items);
 ######删除规则25
 eval(e2Rex(getHttp('https://inmemory.coding.net/p/InMemory/d/MBrowser/git/raw/master/AppFile/js/q.js'),'.dn64()'));
-var title=getVar("title");
+var title=getVar("订阅名");
 var filename=title+'.json';
 var 记录=getVar("CODE");
 var 新记录=JSON.parse(_.read(filename));
