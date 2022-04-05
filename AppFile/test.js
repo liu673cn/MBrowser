789bc05a1042702fd4e9aed5d82eb453c7546309
272775028@qq.com <272775028@qq.com>
2022-04-05 18:26:42 +0800

    update

--- AppFile/test.js
+++ AppFile/test.js
@@ -780,19 +780,17 @@ var ff="本地,#\n"+_.read(filename);
 var code=ff.match(/.+?,.+/g);
 var items=[];
 for (var i in code){
-    if(_.read(Fname)){
-        var title=e2Rex(code[i],".tz(,)");
-        var Fname=title+".json";
-        var Curl=JSON.parse(_.read(Fname));
-        var img=e2Rex(code[i],".ty(#)");
-        if(img){
-            var img=img;
-        }else{
-            var img="http://1.117.152.239:39000/tupian.php?text="+title;
-        }
-        items.push({title:title,img:img,Curl:Curl});
+    var title=e2Rex(code[i],".tz(,)");
+    var Fname=title+".json";
+    var img=e2Rex(code[i],".ty(#)");
+    if(img){
+        var img=img;
     }else{
-        "";
+        var img="http://1.117.152.239:39000/tupian.php?text="+title;
+    }
+    if(_.read(Fname)){
+        var Curl=JSON.parse(_.read(Fname));
+        items.push({title:title,img:img,Curl:Curl});
     }
 }
 JSON.stringify(items);
