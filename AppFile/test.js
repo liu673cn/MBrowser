cf4f89b24bde0348fd8714445ad6591f13acbce8
InMemory <272775028@qq.com>
2022-04-06 20:30:34 +0800

    update

--- AppFile/test.js
+++ AppFile/test.js
@@ -756,7 +756,7 @@ var filename='远程订阅索引.txt';
 if(_.read(filename)){
     var code=_.read(filename).match(/.+?,.+/g);
 }else{
-    var data="InMemory,https://inmemory.coding.net/p/InMemory/d/MBrowser/git/raw/master/AppFile/rule/app.txt#https://inmemory.coding.net/p/InMemory/d/MBrowser/git/raw/master/FileGit/icon/InMemory.jpg";
+    var data="内置,https://inmemory.coding.net/p/InMemory/d/MBrowser/git/raw/master/AppFile/rule/app.txt#";
     _.write(data,filename);
     var code=_.read(filename).match(/.+?,.+/g);
 }
